e75feedef6971e970c558ae4100d9c0d095d8daf
Riccardo Consiglio <47518595+rc2003@users.noreply.github.com>
2019-04-10 20:39:16 +0200

    Add files via upload

--- es.py
+++ es.py
@@ -1,3 +1,4 @@
+
 import requests
 CC=0
 a=requests.post("http://192.168.1.231:8080/accreditamento", json={"nome":"Riccardo Consiglio"})
@@ -138,6 +139,286 @@ a=requests.post("http://192.168.1.231:8080/esercizi/12", json={"data":res})
 CC+=1
 print(CC)
 print(a.json())
+
+#13
+y=requests.get("http://192.168.1.231:8080/esercizi/13", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=[]
+for i in len(d):
+	if i==len(d):
+		res.append(d[i])
+	else:
+		res.append(d[i]+d[i+1]):
+a=requests.post("http://192.168.1.231:8080/esercizi/13", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#14
+y=requests.get("http://192.168.1.231:8080/esercizi/14", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+p=0
+n=0
+z=0
+for i in d:
+	if i > 0:
+		p+=1
+	elif i == 0:
+		z+=1
+	else:
+		n+=1
+res={
+	"positivi":p,
+	"negativi":n,
+	"zeri":z}
+a=requests.post("http://192.168.1.231:8080/esercizi/14", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#15
+y=requests.get("http://192.168.1.231:8080/esercizi/15", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=[]
+for i in d:
+	if len(i)%2==0:
+		res.append(i.upper())
+	else:
+		res.append(i.lower())
+a=requests.post("http://192.168.1.231:8080/esercizi/15", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#16
+y=requests.get("http://192.168.1.231:8080/esercizi/16", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=""
+counter=0
+for i in d:
+	counter+=1
+	if counter==len(d):
+		res+=i
+	else:
+		res+=i+" "
+a=requests.post("http://192.168.1.231:8080/esercizi/16", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#17
+y=requests.get("http://192.168.1.231:8080/esercizi/17", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=""
+for i in d:
+	res+=i[-1]
+a=requests.post("http://192.168.1.231:8080/esercizi/17", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#18
+y=requests.get("http://192.168.1.231:8080/esercizi/18", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=""
+for i in d:
+	if len(i)>4:
+		res+=i[0]
+a=requests.post("http://192.168.1.231:8080/esercizi/18", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#19
+y=requests.get("http://192.168.1.231:8080/esercizi/19", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=[]
+for i in range(d+1):
+	if d%i==0:
+		res.append(i)
+res.sort()
+a=requests.post("http://192.168.1.231:8080/esercizi/19", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#20
+DA FARE
+'''
+y=requests.get("http://192.168.1.231:8080/esercizi/20", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+
+a=requests.post("http://192.168.1.231:8080/esercizi/20", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+'''
+
+#21
+y=requests.get("http://192.168.1.231:8080/esercizi/21", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=[]
+for i in d:
+	if i<=5:
+		res.append(i)
+a=requests.post("http://192.168.1.231:8080/esercizi/21", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#22
+y=requests.get("http://192.168.1.231:8080/esercizi/22", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=[]
+for i in d:
+	if i >=3 and i<=6:
+		res.append(i)
+a=requests.post("http://192.168.1.231:8080/esercizi/22", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#23
+DA FARE
+'''
+y=requests.get("http://192.168.1.231:8080/esercizi/23", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+
+a=requests.post("http://192.168.1.231:8080/esercizi/23", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+'''
+
+#24
+DA FARE
+'''
+y=requests.get("http://192.168.1.231:8080/esercizi/24", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+
+a=requests.post("http://192.168.1.231:8080/esercizi/24", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+'''
+
+#25
+y=requests.get("http://192.168.1.231:8080/esercizi/25", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=0
+for i in d:
+	for x in i:
+		if x=="a":
+			res+=1
+a=requests.post("http://192.168.1.231:8080/esercizi/25", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#26
+y=requests.get("http://192.168.1.231:8080/esercizi/26", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=[]
+for i in d:
+	res.append(i-i*2)
+a=requests.post("http://192.168.1.231:8080/esercizi/26", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#27
+y=requests.get("http://192.168.1.231:8080/esercizi/27", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+res=[]
+for i in d["negozio"]:
+	res.append(i)
+for i in d["magazzino"]:
+	if i not in res:
+		res.append(i)
+a=requests.post("http://192.168.1.231:8080/esercizi/27", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#28
+y=requests.get("http://192.168.1.231:8080/esercizi/28", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+prod=[]
+num=[]
+for i in d["negozio"]:
+	if i not in prod:
+		prod.append(i)
+for i in prod:
+	n=d["negozio"].count(i)
+	m=d["magazzino"].count(i)
+	num.append(n+m)
+tupl=zip(prod, num)
+res=dict(tupl)
+a=requests.post("http://192.168.1.231:8080/esercizi/28", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+#29
+ DA FARE
+ '''
+y=requests.get("http://192.168.1.231:8080/esercizi/29", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+f=d
+
+a=requests.post("http://192.168.1.231:8080/esercizi/29", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+'''
+
+#30
+DA FARE
+'''
+y=requests.get("http://192.168.1.231:8080/esercizi/30", headers={"x-data": "true"})
+c=y.json()
+d=c["data"]
+f=d
+
+a=requests.post("http://192.168.1.231:8080/esercizi/30", json={"data":res})
+CC+=1
+print(CC)
+print(a.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
